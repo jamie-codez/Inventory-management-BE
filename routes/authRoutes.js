90813b335b4991da6221560fac861eb6d6cb33d1
jamie-codez <crusiseomondi90@gmail.com>
2023-04-20 00:03:07 +0300

    Added auth routes to code

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const { login, resetPassword, requestResetPassword, sendPasswordResetpage, refreshJwt } = require("../controllers/authController");
+
+router.post("/login", login);
+router.post("/resetPassword", resetPassword);
+router.post("/requestResetPassword", requestResetPassword);
+router.get("/sendPasswordResetpage", sendPasswordResetpage);
+router.post("/refreshJwt", refreshJwt);
+
+module.exports = router;
