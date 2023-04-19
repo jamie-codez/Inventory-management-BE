2084dc739064634ff1f4aefed23627fed20dccd3
jamie-codez <crusiseomondi90@gmail.com>
2023-04-19 23:17:42 +0300

    Working on userRoutes

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+router.post("/",(req,res)=>{
+
+});
+
+router.get("/",(req,res)=>{
+
+});
+
+router.put("/",(req,res)=>{
+
+});
+
+router.delete("/",(req,res)=>{
+
+});
+
+module.exports = router;
