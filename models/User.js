9e5c57b4e20971d26c273ce0cdd53083dc8279ac
jamie-codez <crusiseomondi90@gmail.com>
2023-04-24 23:12:54 +0300

    Working on user model

--- models/User.js
+++ models/User.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    username: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    password: { type: String, required: true }
+    username: { type: String, required: [true, "Please add a username"] },
+    firstName: { type: String, required: [true, "Please add a first name"] },
+    lastName: { type: String, required: [true, "Please add a last name"] },
+    email: { type: String, required: [true, "Please add an email"], unique: true, trim: true,match:[/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,"Enter a valid email"]},
+    phoneNumber: { type: String, required: [true, "Please add a phone number"] },
+    imageUrl: { type: String, required: [true, "Please add an image url"] },
+    password: { type: String, required: [true, "Please add a password"] }
 });
 
 const model = mongoose.model("User", userSchema);
