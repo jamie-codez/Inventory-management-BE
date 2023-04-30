6e1b513c7352e8bfe36093ad3e5e5a6ca1e5024e
jamie-codez <crusiseomondi90@gmail.com>
2023-04-30 14:50:50 +0300

    Finishing user service

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -17,23 +17,60 @@ const createUser = asyncHandler(async (req, res) => {
     const salt = bcrypt.genSalt(10)
     const encryptedPassword = bcrypt.hash(password, salt);
     const user = await User({ username, firstName, lastName, email, phoneNumber, encryptedPassword });
-    if(!user){
-        return res.status(500).json({code:500,message:"Error occurred could not create user"});
+    if (!user) {
+        return res.status(500).json({ code: 500, message: "Error occurred could not create user" });
     }
-    if(user){
-        return res.status(201).json({code:201,message:"User created succcessfully"});
+    if (user) {
+        return res.status(201).json({ code: 201, message: "User created succcessfully,check email to verify account." });
     }
 });
 
 const getUsers = async (req, res) => {
-    res.send("Hello getUsers");
+    const { pageNumber, pageSize } = req.body;
+    if (!pageNumber) {
+        return res.status(200).json({ code: 400, messaage: "pageNumber cannot be empty" });
+    }
+    if (!pageSize) {
+        return res.status(200).json({ code: 400, message: "pageSize cannot be empty" });
+    }
+    const count = await User.find().count();
+    await User.find().then(users => {
+        res.status(200).json({ code: 200, message: "Successful", payload: users, pagination: { pageNumber: pageNumber, pageSize: pageSize, pages: Number(count) / pageNumber } });
+    });
 }
 
 const updateUser = async (req, res) => {
-    res.send("Hello updateUser");
+    const { id, payload } = req.body
+    if (payload.password) {
+        const salt = bcrypt.genSalt(10);
+        const encryptedPassword = bcrypt.hash(payload.password, salt);
+        req.body.password = encryptedPassword;
+    }
+    await User.findByIdAndUpdate({ "_id": id }, { $set: payload })
+        .then(result => {
+            res.status(200).json({ code: 200, message: "User updated successfully" })
+            console.log(result);
+        })
+        .catch(error => {
+            res.status(500).json({ code: 500, messaage: "Error occurred try again" })
+            console.log(error);
+        });
 }
 
 const deleteUser = async (req, res) => {
+    const { id } = req.body;
+    const user = await User.findOne({ _id: id })
+    if (!user) {
+        return res.status(404).json({ code: 404, message: "User not found" });
+    }
+    await User.findOneAndDelete({ _id: id })
+        .then(result => {
+            res.status(200).json({ code: 200, message: "User deleted successfully" });
+        })
+        .catch(error => {
+            res.status(500).json({ code: 500, messaage: "Error occurred try again" })
+            console.log(error);
+        });
     res.send("Hello deleteUser");
 }
 
