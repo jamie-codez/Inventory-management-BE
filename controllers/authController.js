f58b3bda66b608ebdf9be0194132da63f21b4582
jamie-codez <crusiseomondi90@gmail.com>
2023-05-01 09:55:41 +0300

    Implememted cookies in the login response

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,9 +23,31 @@ const login = asyncHandler(async (req, res) => {
     const authToken = await jwt.sign({ sub: user._id, expiresIn: "7d", iat: Date.now() }, process.env.JWT_SECRET)
     const refreshToken = await jwt.sign({ sub: user._id, expiresIn: "30d", iat: Date.now() }, process.env.JWT_SECRET)
     res.setHeader("Content-Type", "application/json");
-    res.setHeader("access-token", authToken);
-    res.setHeader("refresh-token", refreshToken);
+    res.cookie("access-token",authToken,{
+        path:"/",
+        httpOnly:true,
+        expires:new Date(Date.now()+(1000*3600*24*7)),
+        sameSite:"none",
+        secure:true
+    });
+    res.cookie("refresh-token",refreshToken,{
+        path:"/",
+        httpOnly:true,
+        expires:new Date(Date.now()+(1000*3600*24*30)),
+        sameSite:"none",
+        secure:true
+    });
     res.status(200).json({ code: 200, message: "Login successful" });
+});
+
+const logout = asyncHandler(async(req,res)=>{
+    res.cookie("access-token","",{
+        path:"/",
+        httpOnly:true,
+        expires:new Date(0),
+        sameSite:"none",
+        secure:true
+    });
 });
 
 const resetPassword = asyncHandler(async (req, res) => {
@@ -82,12 +104,16 @@ const refreshJwt = asyncHandler(async (req, res) => {
     if (decodedJwt.expiresIn <= Date.now()) {
         const authToken = await jwt.sign({ sub: user._id, expiresIn: "7d", iat: Date.now() }, process.env.JWT_SECRET)
         res.setHeader("Content-Type", "application/json");
-        res.setHeader("access-token", authToken); //3600*24*7
-        //TODO: Resolve to using httpOnly cookies
-        // res.cookie("token",token,{path:"/",httpOnly:true,expires:new Date(Date.now() + 1000 *86400),sameSite:"none",secure:true})
+        res.cookie("access-token",authToken,{
+            path:"/",
+            httpOnly:true,
+            expires:new Date(Date.now()+(1000*3600*24*7)),
+            sameSite:"none",
+            secure:true
+        });
         return res.status(200).json({ code: 200, message: "Token refreshed succesddfully." })
     }
     res.status(403).setHeader("Content-Type", "application/json").json({ code: 403, message: "Refresh token expired you need to login again" });
 });
 
-module.exports = { login, resetPassword, requestResetPassword, sendPasswordResetpage, refreshJwt };
+module.exports = { login, logout,resetPassword, requestResetPassword, sendPasswordResetpage, refreshJwt };
